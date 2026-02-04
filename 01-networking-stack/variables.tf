ed9456ef7e5872adc13d0d44aa0c957c0a237971
André Borghi <andreborghi97@gmail.com>
2026-02-04 12:04:48 -0300

    feat: Adding VPC and new network structures

--- 01-networking-stack/variables.tf
+++ 01-networking-stack/variables.tf
@@ -23,12 +23,52 @@ variable "assume_role" {
 
 variable "vpc" {
   type = object({
-    name       = string
-    cidr_block = string
+    name                     = string
+    cidr_block               = string
+    internet_gateway_name    = string
+    nat_gateway_name         = string
+    public_route_table_name  = string
+    private_route_table_name = string
+    public_subnets = list(object({
+      name              = string
+      cidr_block        = string
+      availability_zone = string
+    }))
+    private_subnets = list(object({
+      name              = string
+      cidr_block        = string
+      availability_zone = string
+    }))
   })
 
   default = {
-    name       = "vpc"
-    cidr_block = "10.0.0.0/24"
+    name                     = "vpc"
+    cidr_block               = "10.0.0.0/24"
+    internet_gateway_name    = "igw"
+    nat_gateway_name         = "nat-gateway"
+    public_route_table_name  = "public-route-table"
+    private_route_table_name = "private-route-table"
+    public_subnets = [{
+      name              = "vpc-public-subnet-us-east-1a"
+      cidr_block        = "10.0.0.0/26"
+      availability_zone = "us-east-1a"
+      },
+      {
+        name              = "vpc-public-subnet-us-east-1b"
+        cidr_block        = "10.0.0.64/26"
+        availability_zone = "us-east-1b"
+    }]
+
+    private_subnets = [{
+      name              = "vpc-private-subnet-us-east-1a"
+      cidr_block        = "10.0.0.128/26"
+      availability_zone = "us-east-1a"
+      },
+      {
+        name              = "vpc-private-subnet-us-east-1b"
+        cidr_block        = "10.0.0.192/26"
+        availability_zone = "us-east-1b"
+    }]
+
   }
 }
